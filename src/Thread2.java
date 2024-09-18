ff181d0b736c380480dde87fc938e499aa6a89c8
Vishwajeet <singhvishwajeet1998@gmail.com>
2024-09-18 16:10:02 +0530

    Thread creation using Thread Class

--- src/Thread2.java
+++ src/Thread2.java
@@ -1,5 +1,11 @@
 public class Thread2 {
 
+    public static class NewThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
+    }
     public static void main(String[] args) {
 
     }
